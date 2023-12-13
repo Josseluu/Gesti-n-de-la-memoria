98cca76be1a1361f0e2e0ce5d5b919b89482791d
Josseluu <jslslpz@myuax.com>
2023-12-13 18:55:09 +0100

    Initial commit

--- Gestión de la memoria.cpp
+++ Gestión de la memoria.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 #define SIZE 4096
 int main() {
     void *shared_memory = static_cast<char *>(mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1,
